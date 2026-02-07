cab5d2fa52cb2c3dd5897b7f0fb7d6e17ec0aa72
roman.amelin <roman.amelin@x5.ru>
2026-02-07 14:16:32 +0300

    update

--- postgres/init/01-init.sql
+++ postgres/init/01-init.sql
@@ -3,3 +3,6 @@ CREATE DATABASE unibpm OWNER unibpm;
 
 CREATE USER camunda PASSWORD 'camunda';
 CREATE DATABASE camunda OWNER camunda;
+
+CREATE USER keycloak PASSWORD 'keycloak'
+CREATE DATABASE keycloak OWNER keycloak;
