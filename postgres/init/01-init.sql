817438d7586d6f9aaec2ce496d2f770f26873db1
roman.amelin <roman.amelin@x5.ru>
2026-02-07 14:29:19 +0300

    update

--- postgres/init/01-init.sql
+++ postgres/init/01-init.sql
@@ -1,8 +1,33 @@
-CREATE USER unibpm PASSWORD 'unibpm';
-CREATE DATABASE unibpm OWNER unibpm;
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'unibpm') THEN
+    CREATE ROLE unibpm LOGIN PASSWORD 'unibpm';
+  END IF;
 
-CREATE USER camunda PASSWORD 'camunda';
-CREATE DATABASE camunda OWNER camunda;
+  IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'camunda') THEN
+    CREATE ROLE camunda LOGIN PASSWORD 'camunda';
+  END IF;
 
-CREATE USER keycloak PASSWORD 'keycloak'
-CREATE DATABASE keycloak OWNER keycloak;
+  IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'keycloak') THEN
+    CREATE ROLE keycloak LOGIN PASSWORD 'keycloak';
+  END IF;
+END $$;
+
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'unibpm') THEN
+    CREATE DATABASE unibpm OWNER unibpm;
+  END IF;
+
+  IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'camunda') THEN
+    CREATE DATABASE camunda OWNER camunda;
+  END IF;
+
+  IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'keycloak') THEN
+    CREATE DATABASE keycloak OWNER keycloak;
+  END IF;
+END $$;
+
+GRANT ALL PRIVILEGES ON DATABASE unibpm TO unibpm;
+GRANT ALL PRIVILEGES ON DATABASE camunda TO camunda;
+GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
