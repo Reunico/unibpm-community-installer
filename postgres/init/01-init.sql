005f61ffdc9b60d99b606adb01d48de030a751f3
roman.amelin <roman.amelin@x5.ru>
2026-02-07 14:37:48 +0300

    update

--- postgres/init/01-init.sql
+++ postgres/init/01-init.sql
@@ -1,23 +1,7 @@
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'unibpm') THEN
-    CREATE ROLE unibpm LOGIN PASSWORD 'unibpm';
-  END IF;
+CREATE ROLE unibpm  LOGIN PASSWORD 'unibpm';
+CREATE ROLE camunda LOGIN PASSWORD 'camunda';
+CREATE ROLE keycloak LOGIN PASSWORD 'keycloak';
 
-  IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'camunda') THEN
-    CREATE ROLE camunda LOGIN PASSWORD 'camunda';
-  END IF;
-
-  IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'keycloak') THEN
-    CREATE ROLE keycloak LOGIN PASSWORD 'keycloak';
-  END IF;
-END $$;
-
-SELECT format('CREATE DATABASE %I OWNER %I', 'unibpm', 'unibpm')
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'unibpm') \gexec;
-
-SELECT format('CREATE DATABASE %I OWNER %I', 'camunda', 'camunda')
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'camunda') \gexec;
-
-SELECT format('CREATE DATABASE %I OWNER %I', 'keycloak', 'keycloak')
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'keycloak') \gexec;
+CREATE DATABASE unibpm  OWNER unibpm;
+CREATE DATABASE camunda OWNER camunda;
+CREATE DATABASE keycloak OWNER keycloak;
